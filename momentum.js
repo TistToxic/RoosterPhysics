5d0d1102274d23f638c723667671d05e4f0a9cd4
Patthon Rayubsang <tistplays584yt@gmail.com>
2026-02-14 12:17:41 +0700

    Revamp & new plans

--- momentum.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.getElementById("screen");
-const ctx = canvas.getContext("2d");
-const canvasRect = canvas.getBoundingClientRect();
-
-canvas.addEventListener("click", (e) => {
-    console.log(e.x - canvasRect.left, e.y - canvasRect.top);
-});
